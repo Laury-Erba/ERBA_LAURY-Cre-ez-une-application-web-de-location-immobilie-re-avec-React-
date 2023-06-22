733f4626b143be0102669b3e533f002a415e625b
laury <lauryerba@hotmail.fr>
2023-06-22 14:47:26 +0200

    Card

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import routes from "./Components/Routes"
 import "./Styles/index.css"
 
-<<<<<<< HEAD
+
  //charge les routes pour les différentes pages, ainsi que la page d'erreur
-=======
-// charge les routes pour les différentes pages, ainsi que la page d'erreur
->>>>>>> 11d29a260ea346811755d6da06303a6621da48a3
+
 routes();
